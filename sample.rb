2633731bacc875b9d9933e26699a0c56b6b247a4
Mami_MASUDA <mami_masuda@icloud.com>
2022-07-06 11:41:01 +0900

    Phase6-2

--- sample.rb
+++ sample.rb
@@ -1,8 +1,23 @@
-number = gets.to_i
-if number < 1
+def input_value
+
+count = 1
+
+while count <= 10
+value = gets
+puts value
+exit
+if value < 1
   puts "エラーです"
-elsif number > 5
-  puts "エラーです"
+elsif value > 5
+  puts  "エラーです"
 else
   puts "ありがとうございます"
 end
+
+count = count + 1
+
+end
+
+end
+
+input_value
